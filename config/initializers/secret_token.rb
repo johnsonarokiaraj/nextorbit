fa8014dbe98dd8f31686bc25937777fa23bbf3f2
johnsonarokiaraj <johnsonarokiaraj@gmail.com>
2014-08-07 21:01:17 +0530

    New rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nextorbit::Application.config.secret_key_base = '05c6f9c9ebe22928f9e8641fd44e401a5eeb7a794671cad5dd1cf2752d3403d192dbe5fe32e47f5cf2c643ca13ff4c882b9dc7aa5f4e71b8b3301db61de39bc2'
