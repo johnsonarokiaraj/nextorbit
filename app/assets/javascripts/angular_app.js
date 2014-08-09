811cdb8d9dbf886ab4b264d987961d41cebda04d
johnsonarokiaraj <johnsonarokiaraj@gmail.com>
2014-08-09 12:51:03 +0530

    Loading part implemented

--- app/assets/javascripts/angular_app.js
+++ app/assets/javascripts/angular_app.js
@@ -14,15 +14,18 @@
         $scope.weathers = [];
         $scope.call_weather = function(station){
             console.log(station);
+            $('.loading').show();
             station.is_selected =!station.is_selected;
             if(station.is_selected){
                 $http.get('/data?lat='+station.lat+'&long='+station.long).success(function(data){
+                    $('.loading').hide();
                     console.log(data);
                     $scope.weathers.push(data);
                 });
             }
             else{                
                $scope.weathers = _.reject($scope.weathers, function (weather) {
+                   $('.loading').hide();
                    return weather.coord.lat == station.lat.toFixed(2) &&weather.coord.lon == station.long.toFixed(2);
                });
             }
